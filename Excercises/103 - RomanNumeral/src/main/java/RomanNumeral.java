488dc196b1cbcbc55981c5edda322478768b8d8d
Antonio Gómez Márquez <soft.agomezm@iberia.es>
2022-10-18 11:57:50 +0200

    feat - return number for V and X values

--- Excercises/103 - RomanNumeral/src/main/java/RomanNumeral.java
+++ Excercises/103 - RomanNumeral/src/main/java/RomanNumeral.java
@@ -1,5 +1,15 @@
 public class RomanNumeral {
     public int convert(RomanValues i) {
-        return 1;
+        switch (i) {
+            case I : return 1;
+            case V: return 5;
+            case X: return 10;
+            case L:
+            case C:
+            case D:
+            case M:
+            default:
+                return 0;
+        }
     }
 }
--- Excercises/103 - RomanNumeral/src/test/java/RomanNumeralTest.java
+++ Excercises/103 - RomanNumeral/src/test/java/RomanNumeralTest.java
@@ -1,14 +1,27 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class RomanNumeralTest {
 
-    @Test
-    public void shouldReturnOne() {
+    private static Stream<Arguments> romanSimpleValues() {
+        return Stream.of(
+                Arguments.of(RomanValues.I, 1),
+                Arguments.of(RomanValues.V, 5),
+                Arguments.of(RomanValues.X, 10)
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("romanSimpleValues")
+    public void shouldReturnNumberForSimpleValue(RomanValues romanValue, int number) {
         //given
         RomanNumeral romanNumeral = new RomanNumeral();
         //when and then
-        Assertions.assertEquals(1, romanNumeral.convert(RomanValues.I));
+        Assertions.assertEquals(number, romanNumeral.convert(romanValue));
 
     }
 }
