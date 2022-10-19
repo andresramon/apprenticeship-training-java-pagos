401068b7762a32603fac7c3de3d48e059f1d4144
ssubijana01 <AUTEN.ssubijana@iberia.es>
2022-10-19 08:46:44 +0200

    feat (WIP) - convert roman numbers

--- Excercises/103 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/RomanConverter.java
+++ Excercises/103 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/RomanConverter.java
@@ -1,27 +1,23 @@
 package org.cokaido.apprenticeship;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RomanConverter {
     public int convert(RomanNumeral romanValue) throws InvalidRomanNumeralException{
-        if(romanValue == null){
-            throw new InvalidRomanNumeralException();
-        }
-        switch (romanValue) {
-            case I : return 1;
-            case V: return 5;
-            case X: return 10;
-            case L: return 50;
-            case C: return 100;
-            case D: return 500;
-            case M: return 1000;
-            default:
-                return 0;
-        }
+        return RomanNumeral.getValue(romanValue);
     }
 
     public int convert(String romanValue) throws InvalidRomanNumeralException{
         if(romanValue == null){
             throw new InvalidRomanNumeralException();
         }
+        final List<Character> romanNumbers = romanValue.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+
+        //TODO: Add foreach
+
+
         if(romanValue.equals("II")){
             return 2;
         }
--- Excercises/103 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/RomanNumeral.java
+++ Excercises/103 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/RomanNumeral.java
@@ -1,5 +1,19 @@
 package org.cokaido.apprenticeship;
 
 public enum RomanNumeral {
-    I, V, X, L, C, D, M
+    I, V, X, L, C, D, M;
+
+    public static int getValue(final RomanNumeral romanNumeral){
+        switch (romanNumeral) {
+        case I : return 1;
+        case V: return 5;
+        case X: return 10;
+        case L: return 50;
+        case C: return 100;
+        case D: return 500;
+        case M: return 1000;
+        default:
+            return 0;
+        }
+    }
 }
