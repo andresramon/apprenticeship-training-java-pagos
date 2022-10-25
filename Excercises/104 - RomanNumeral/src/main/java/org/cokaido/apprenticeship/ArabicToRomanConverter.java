2dbb7be58e701800a1eec04253e278db7d7b5434
Antonio Gómez Márquez <soft.agomezm@iberia.es>
2022-10-25 11:36:30 +0200

    refact - use dictionary

--- Excercises/104 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/ArabicToRomanConverter.java
+++ Excercises/104 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/ArabicToRomanConverter.java
@@ -2,13 +2,6 @@ package org.cokaido.apprenticeship;
 
 public class ArabicToRomanConverter {
     public String convert(int arabicNumber) {
-        if (arabicNumber==1) {
-            return "I";
-        }
-        if (arabicNumber==2) {
-            return "II";
-        }
-
-        return "III";
+        return RomanNumeral.fromArabicNumeral(arabicNumber).name();
     }
 }
--- Excercises/104 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/RomanNumeral.java
+++ Excercises/104 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/RomanNumeral.java
@@ -1,7 +1,9 @@
 package org.cokaido.apprenticeship;
 
+import java.util.Arrays;
+
 public enum RomanNumeral {
-    I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), XM(900), M(1000);
+    I(1), II(2), III(3),IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), XM(900), M(1000);
 
     private final int value;
 
@@ -11,6 +13,11 @@ public enum RomanNumeral {
 
     private int getValue() {
         return value;
+    }
+
+    public static RomanNumeral fromArabicNumeral(int arabicNumeral) {
+        return Arrays.stream(values()).filter(romanNumeral -> romanNumeral.value == arabicNumeral).findFirst().
+                orElse(null);
     }
 
     public static int getValue(RomanNumeral romanNumeral) {
