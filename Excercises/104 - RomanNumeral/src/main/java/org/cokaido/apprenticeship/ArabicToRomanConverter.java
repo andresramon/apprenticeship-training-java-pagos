562d0c967052226410557126aab6124efcb251db
Miguel Angel Davila <madavila@viewnext.com>
2022-10-25 12:37:52 +0200

    feat - add arabic numeral conversion from 14 to 41

--- Excercises/104 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/ArabicToRomanConverter.java
+++ Excercises/104 - RomanNumeral/src/main/java/org/cokaido/apprenticeship/ArabicToRomanConverter.java
@@ -3,11 +3,21 @@ package org.cokaido.apprenticeship;
 public class ArabicToRomanConverter {
 
     public String convert(int arabicNumber) {
-
         RomanNumeral romanNumeral = RomanNumeral.fromArabicNumeral(arabicNumber);
         if (romanNumeral != null) {
             return romanNumeral.name();
         }
+
+        if (arabicNumber> 50) {
+            return "L"+ convert(arabicNumber-50);
+        }
+        if (arabicNumber> 40) {
+            return "XL"+ convert(arabicNumber-40);
+        }
+        if (arabicNumber> 10) {
+            return "X"+ convert(arabicNumber-10);
+        }
+
         return convert(arabicNumber-1) + "I";
 
     }
--- Excercises/104 - RomanNumeral/src/test/java/org/cokaido/apprenticeship/ArabicToRomanConverterTest.java
+++ Excercises/104 - RomanNumeral/src/test/java/org/cokaido/apprenticeship/ArabicToRomanConverterTest.java
@@ -16,7 +16,13 @@ public class ArabicToRomanConverterTest {
                 Arguments.of(2,"II"),
                 Arguments.of(3,"III"),
                 Arguments.of(4, "IV"),
-                Arguments.of(8, "VIII")
+                Arguments.of(8, "VIII"),
+                Arguments.of(11, "XI"),
+                Arguments.of(14, "XIV"),
+                Arguments.of(39, "XXXIX"),
+                Arguments.of(40, "XL"),
+                Arguments.of(41, "XLI")
+               //, Arguments.of(59, "LIX")
         );
 
     }
