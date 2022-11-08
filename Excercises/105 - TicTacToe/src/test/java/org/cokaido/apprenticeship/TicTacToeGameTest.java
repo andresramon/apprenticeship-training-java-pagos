68dc4284c9e42f0c597dd83809d11875c23351ca
Antonio Gómez Márquez <soft.agomezm@iberia.es>
2022-11-08 11:46:42 +0100

    test - add all rows scenaries

--- Excercises/105 - TicTacToe/src/test/java/org/cokaido/apprenticeship/TicTacToeGameTest.java
+++ Excercises/105 - TicTacToe/src/test/java/org/cokaido/apprenticeship/TicTacToeGameTest.java
@@ -3,6 +3,12 @@ package org.cokaido.apprenticeship;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public class TicTacToeGameTest{
 
@@ -53,14 +59,37 @@ public class TicTacToeGameTest{
         Assertions.assertThrows(InvalidOperationException.class, () -> game.play(PLAYER_O, position));
     }
 
-    @Test
-    public void gameShouldBeOverWhenRowHasBeenTakenByPlayer()
+    public static Stream<Arguments> winnerRowCombinations() {
+        return Stream.of(
+                Arguments.of(List.of(GamePlays.PositionType.TOP_LEFT,
+                        GamePlays.PositionType.CENTER_LEFT,
+                        GamePlays.PositionType.TOP_CENTER,
+                        GamePlays.PositionType.BOTTOM_CENTER,
+                        GamePlays.PositionType.TOP_RIGHT)),
+                Arguments.of(List.of(GamePlays.PositionType.CENTER_LEFT,
+                        GamePlays.PositionType.TOP_LEFT,
+                        GamePlays.PositionType.CENTER,
+                        GamePlays.PositionType.BOTTOM_CENTER,
+                        GamePlays.PositionType.CENTER_RIGHT)),
+                Arguments.of(List.of(GamePlays.PositionType.BOTTOM_LEFT,
+                        GamePlays.PositionType.CENTER_LEFT,
+                        GamePlays.PositionType.BOTTOM_CENTER,
+                        GamePlays.PositionType.TOP_CENTER,
+                        GamePlays.PositionType.BOTTOM_RIGHT))
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("winnerRowCombinations")
+    public void gameShouldBeOverWhenRowHasBeenTakenByPlayer(List<GamePlays.PositionType> positionTypeList)
             throws InvalidPlayerException, InvalidOperationException, GameOverException{
-        game.play(PLAYER_X, GamePlays.PositionType.TOP_LEFT);
-        game.play(PLAYER_O, GamePlays.PositionType.CENTER_LEFT);
-        game.play(PLAYER_X, GamePlays.PositionType.TOP_CENTER);
-        game.play(PLAYER_O, GamePlays.PositionType.BOTTOM_CENTER);
-        game.play(PLAYER_X, GamePlays.PositionType.TOP_RIGHT);
+        game.play(PLAYER_X, positionTypeList.get(0));
+        game.play(PLAYER_O, positionTypeList.get(1));
+        game.play(PLAYER_X, positionTypeList.get(2));
+        game.play(PLAYER_O, positionTypeList.get(3));
+        game.play(PLAYER_X, positionTypeList.get(4));
         Assertions.assertThrows(GameOverException.class, () -> game.play(PLAYER_O, GamePlays.PositionType.BOTTOM_RIGHT));
     }
+
 }
