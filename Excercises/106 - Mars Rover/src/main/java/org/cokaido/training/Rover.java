547d07c586a7f5f14d42b3d2fd366c6be2e0c06e
Miguel Angel Davila <madavila@viewnext.com>
2022-11-15 12:47:38 +0100

    refact - remove duplications on execute

--- Excercises/106 - Mars Rover/src/main/java/org/cokaido/training/Rover.java
+++ Excercises/106 - Mars Rover/src/main/java/org/cokaido/training/Rover.java
@@ -1,35 +1,47 @@
 package org.cokaido.training;
 
+import java.util.StringJoiner;
+
 public class Rover{
 
-    public enum CardinalPoints{
-        N, S, E, O
+    public enum CardinalPoints {
+        N(0, 1), S(0, -1), E(1, 0), O(-1, 0);
+
+        private final int xAxis;
+        private final int yAxis;
+
+        CardinalPoints(int xAxis, int yAxis) {
+            this.xAxis = xAxis;
+            this.yAxis = yAxis;
+        }
+
+        public int getXAxis() {
+            return xAxis;
+        }
+
+        public int getYAxis() {
+            return yAxis;
+        }
     }
 
     public String execute(String command){
         final String[] splitCommand = command.split("\n");
         final String[] initialPosition = splitCommand[1].split(" ");
 
-        if (isFacing(CardinalPoints.E, initialPosition[2])) {
-            final int nextPosition = Integer.parseInt(initialPosition[0]) + 1;
-            return nextPosition+ " " + initialPosition[1] + " "+ initialPosition[2];
+        String[] finalPosition = calculatePosition(initialPosition);
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        for (String position : finalPosition) {
+            stringJoiner.add(position);
         }
-
-        if (isFacing(CardinalPoints.O, initialPosition[2])) {
-            final int nextPosition = Integer.parseInt(initialPosition[0]) - 1;
-            return nextPosition+ " " + initialPosition[1] + " "+ initialPosition[2];
-        }
-
-        if (isFacing(CardinalPoints.S, initialPosition[2])) {
-            final int nextPosition = Integer.parseInt(initialPosition[1]) - 1;
-            return initialPosition[0]+ " " + nextPosition + " "+ initialPosition[2];
-        }
-        final int nextPosition = Integer.parseInt(initialPosition[1]) + 1;
-
-        return initialPosition[0]+ " " + nextPosition + " "+ initialPosition[2];
+        return stringJoiner.toString();
+    }
+    
+    private String[] calculatePosition(String[] initialPosition) {
+        CardinalPoints cardinalPoint = CardinalPoints.valueOf(initialPosition[2]);
+        final int nextPositionX = Integer.parseInt(initialPosition[0]) + cardinalPoint.getXAxis();
+        final int nextPositionY = Integer.parseInt(initialPosition[1]) + cardinalPoint.getYAxis();
+        return new String[]{String.valueOf(nextPositionX), String.valueOf(nextPositionY), initialPosition[2]};
     }
 
-    private boolean isFacing(CardinalPoints cardinalPoints, String facing) {
-        return cardinalPoints.name().equals(facing);
-    }
+
 }
