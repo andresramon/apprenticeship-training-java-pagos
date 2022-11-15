854dc8b75ad2f917aa8df3020900b677a8e1f106
ssubijana01 <AUTEN.ssubijana@iberia.es>
2022-11-15 11:55:36 +0100

    feat - move forward facing north

--- Excercises/106 - Mars Rover/src/main/java/org/cokaido/training/Rover.java
+++ Excercises/106 - Mars Rover/src/main/java/org/cokaido/training/Rover.java
@@ -1,2 +1,12 @@
-package org.cokaido.training;public class Rover {
+package org.cokaido.training;
+
+public class Rover{
+    public String execute(String command){
+        final String[] splitCommand = command.split("\n");
+        final String[] initialPosition = splitCommand[1].split(" ");
+
+        final int nextPosition = Integer.parseInt(initialPosition[1]) + 1;
+
+        return "0 " + nextPosition + " N";
+    }
 }
--- Excercises/106 - Mars Rover/src/test/java/org/cokaido/training/RoverTest.java
+++ Excercises/106 - Mars Rover/src/test/java/org/cokaido/training/RoverTest.java
@@ -1,2 +1,28 @@
-package org.cokaido.training;public class RovertTest {
+package org.cokaido.training;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class RoverTest{
+
+    private static Stream<Arguments> argumentsMoveForward(){
+        return Stream.of(
+                Arguments.of("5 5\n0 1 N\nA\n", "0 2 N"),
+                Arguments.of("5 5\n0 2 N\nA\n", "0 3 N")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsMoveForward")
+    public void shouldMoveForward(String command, String expectedPosition){
+        final Rover rover = new Rover();
+
+        String position = rover.execute(command);
+
+        Assertions.assertEquals(expectedPosition, position);
+    }
 }
