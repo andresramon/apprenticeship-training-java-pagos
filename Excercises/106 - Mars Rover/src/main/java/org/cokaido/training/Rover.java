fd24025ac412a4263b538d35a09f62ab8048121d
Antonio Gómez Márquez <soft.agomezm@iberia.es>
2022-11-17 15:49:41 +0100

    refact - readibility changes

--- Excercises/106 - Mars Rover/src/main/java/org/cokaido/training/Rover.java
+++ Excercises/106 - Mars Rover/src/main/java/org/cokaido/training/Rover.java
@@ -4,17 +4,25 @@ import java.util.StringJoiner;
 
 public class Rover{
 
+    private static final String NORTH = "N";
+    private static final String WEST = "O";
+    private static final String EAST = "E";
+    private static final String SOUTH = "S";
+    private static final String FORWARD = "A";
+    private static final String LEFT = "I";
+    private static final String BACKWARD = "R";
+
     public String execute(String command){
         final String[] splitCommand = command.split("\n");
         final String[] initialPosition = splitCommand[1].split(" ");
 
-        if(splitCommand[2].equals("A")) {
+        if(splitCommand[2].equals(FORWARD)) {
             return formatPosition(calculatePosition(initialPosition, true));
         }
-        if(splitCommand[2].equals("I")) {
+        if(splitCommand[2].equals(LEFT)) {
             return formatPosition(rotateLeft(initialPosition));
         }
-        if(splitCommand[2].equals("R")) {
+        if(splitCommand[2].equals(BACKWARD)) {
             return formatPosition(calculatePosition(initialPosition, false));
         }
         return "";
@@ -33,27 +41,35 @@ public class Rover{
     }
 
     private String getLeftOrientation(String orientation) {
-        if(orientation.equals("N")) {
-            return "O";
+        if(orientation.equals(NORTH)) {
+            return WEST;
         }
-        if(orientation.equals("S")) {
-            return "E";
+        if(orientation.equals(SOUTH)) {
+            return EAST;
         }
-        if(orientation.equals("E")) {
-            return "N";
+        if(orientation.equals(EAST)) {
+            return NORTH;
         }
-        return "S";
+        return SOUTH;
     }
 
     private String[] calculatePosition(String[] initialPosition, boolean forward) {
         CardinalPoints cardinalPoint = CardinalPoints.valueOf(initialPosition[2]);
         if(forward){
-            final int nextPositionX = Integer.parseInt(initialPosition[0]) + cardinalPoint.getXAxis();
-            final int nextPositionY = Integer.parseInt(initialPosition[1]) + cardinalPoint.getYAxis();
-            return new String[] {String.valueOf(nextPositionX), String.valueOf(nextPositionY), initialPosition[2]};
+            return moveForward(initialPosition, cardinalPoint);
         }
+        return moveBackward(initialPosition, cardinalPoint);
+    }
+
+    private String[] moveBackward(String[] initialPosition, CardinalPoints cardinalPoint) {
         final int nextPositionX = Integer.parseInt(initialPosition[0]) - cardinalPoint.getXAxis();
         final int nextPositionY = Integer.parseInt(initialPosition[1]) - cardinalPoint.getYAxis();
+        return new String[] {String.valueOf(nextPositionX), String.valueOf(nextPositionY), initialPosition[2]};
+    }
+
+    private String[] moveForward(String[] initialPosition, CardinalPoints cardinalPoint) {
+        final int nextPositionX = Integer.parseInt(initialPosition[0]) + cardinalPoint.getXAxis();
+        final int nextPositionY = Integer.parseInt(initialPosition[1]) + cardinalPoint.getYAxis();
         return new String[] {String.valueOf(nextPositionX), String.valueOf(nextPositionY), initialPosition[2]};
     }
 
